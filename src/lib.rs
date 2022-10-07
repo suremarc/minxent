7a73ac1bf2536a2d045ed9713a65fb8117fe4847
suremarc <8771538+suremarc@users.noreply.github.com>
2022-10-07 02:19:26 -0400

    it doesn't work...

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ impl<'a, S: SampleSpace> SampleSpace for &'a S {
     type Point = S::Point;
 }
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 pub struct Standard<T: Copy + Clone>(std::marker::PhantomData<T>)
 where
     rand::distributions::Standard: Distribution<T>;
@@ -37,18 +37,22 @@ where
 #[derive(Clone)]
 pub struct Exponential<Stat, Space: SampleSpace, const N: usize>
 where
-    Stat: Fn(Space::Point) -> SVector<f64, N>,
+    Stat: Fn(Space::Point) -> SVector<f64, N> + Clone,
+    Space: Clone,
+    [(); N + 1]:,
 {
-    multipliers: SVector<f64, N>,
-    stat: Stat,
-    space: Space,
+    pub multipliers: SVector<f64, { N + 1 }>,
+    pub stat: Stat,
+    pub space: Space,
 }
 
 impl<Stat, Space: SampleSpace, const N: usize> Exponential<Stat, Space, N>
 where
-    Stat: Fn(Space::Point) -> SVector<f64, N>,
+    Stat: Fn(Space::Point) -> SVector<f64, N> + Clone,
+    Space: Clone,
+    [(); N + 1]:,
 {
-    pub fn new(multipliers: SVector<f64, N>, stat: Stat, space: Space) -> Self {
+    pub fn new(multipliers: SVector<f64, { N + 1 }>, stat: Stat, space: Space) -> Self {
         Self {
             multipliers,
             stat,
@@ -56,8 +60,34 @@ where
         }
     }
 
+    pub fn solve(
+        stat: Stat,
+        space: Space,
+        constraints: SMatrix<f64, N, 2>,
+    ) -> Result<Self, ipopt::CreateError> {
+        let problem = Problem {
+            stat: stat.clone(),
+            space: space.clone(),
+            g_l: constraints.column(0).clone_owned(),
+            g_u: constraints.column(1).clone_owned(),
+        };
+
+        let mut ipopt_problem = ipopt::Ipopt::new(problem)?;
+        ipopt_problem.set_option("print_level", 5);
+        ipopt_problem.set_option("obj_scaling_factor", -1.);
+        ipopt_problem.set_option("tol", 1e3);
+
+        let solve_result = ipopt_problem.solve();
+
+        let x = solve_result.solver_data.solution.primal_variables;
+        let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
+        multipliers.copy_from_slice(x);
+
+        Ok(Self::new(multipliers, stat, space))
+    }
+
     pub fn density(&self, x: Space::Point) -> f64 {
-        (self.multipliers.dot(&(self.stat)(x))).exp()
+        (self.multipliers.dot(&self.stat_m1(x))).exp()
     }
 
     pub fn integral<R: Rng, O: std::iter::Sum>(
@@ -78,26 +108,42 @@ where
     }
 
     pub fn entropy<R: Rng>(&self, iterations: usize, rng: &mut R) -> f64 {
-        self.integral(|x| self.multipliers.dot(&(self.stat)(x)), iterations, rng)
+        -self.integral(|x| self.multipliers.dot(&self.stat_m1(x)), iterations, rng)
     }
 
-    pub fn entropy_gradient<R: Rng>(&self, iterations: usize, rng: &mut R) -> SVector<f64, N> {
-        self.integral(
-            |x| (1. + self.multipliers.dot(&(self.stat)(x))) * (self.stat)(x),
+    pub fn entropy_gradient<R: Rng>(
+        &self,
+        iterations: usize,
+        rng: &mut R,
+    ) -> SVector<f64, { N + 1 }> {
+        -self.integral(
+            |x| (1. + self.multipliers.dot(&self.stat_m1(x))) * self.stat_m1(x),
             iterations,
             rng,
         )
     }
 
-    pub fn entropy_hessian<R: Rng>(&self, iterations: usize, rng: &mut R) -> SMatrix<f64, N, N> {
-        self.integral(
+    pub fn entropy_hessian<R: Rng>(
+        &self,
+        iterations: usize,
+        rng: &mut R,
+    ) -> SMatrix<f64, { N + 1 }, { N + 1 }> {
+        -self.integral(
             |x| {
-                let stat = (self.stat)(x);
+                let stat = self.stat_m1(x);
                 (2. + self.multipliers.dot(&stat)) * stat * stat.transpose()
             },
             iterations,
             rng,
         )
+    }
+
+    fn stat_m1(&self, x: Space::Point) -> SVector<f64, { N + 1 }> {
+        let stat = (self.stat)(x);
+        let mut new: SVector<f64, { N + 1 }> = SVector::zeros();
+        new.as_mut_slice()[0..N].copy_from_slice(stat.as_slice());
+        new[N] = -1.;
+        new
     }
 }
 
@@ -111,21 +157,7 @@ where
     g_u: SVector<f64, N>,
 }
 
-impl<Stat, Space: SampleSpace, const N: usize> Problem<Stat, Space, N>
-where
-    Stat: Fn(Space::Point) -> SVector<f64, N>,
-    [(); N + 1]:,
-{
-    fn stat_p1(&self) -> impl Fn(Space::Point) -> SVector<f64, { N + 1 }> + '_ {
-        |x| {
-            let stat = (self.stat)(x);
-            let mut new: SVector<f64, { N + 1 }> = SVector::zeros();
-            new.copy_from_slice(stat.as_slice());
-            new[N] = -1.;
-            new
-        }
-    }
-}
+const ITERATIONS: usize = 10_000;
 
 impl<Stat, Space: SampleSpace, const N: usize> ipopt::BasicProblem for Problem<Stat, Space, N>
 where
@@ -136,8 +168,10 @@ where
         N + 1
     }
 
-    fn bounds(&self, _x_l: &mut [ipopt::Number], _x_u: &mut [ipopt::Number]) -> bool {
-        false
+    fn bounds(&self, x_l: &mut [ipopt::Number], x_u: &mut [ipopt::Number]) -> bool {
+        x_l.fill(-f64::INFINITY);
+        x_u.fill(f64::INFINITY);
+        true
     }
 
     fn initial_point(&self, _x: &mut [ipopt::Number]) -> bool {
@@ -147,18 +181,21 @@ where
     fn objective(&self, x: &[ipopt::Number], obj: &mut ipopt::Number) -> bool {
         let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
         multipliers.copy_from_slice(x);
-        let exp = Exponential::new(multipliers, self.stat_p1(), &self.space);
+        let exp = Exponential::new(multipliers, &self.stat, &self.space);
 
-        *obj = exp.entropy(10_000, &mut thread_rng());
+        *obj = exp.entropy(ITERATIONS, &mut thread_rng());
         true
     }
 
     fn objective_grad(&self, x: &[ipopt::Number], grad_f: &mut [ipopt::Number]) -> bool {
         let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
         multipliers.copy_from_slice(x);
-        let exp = Exponential::new(multipliers, self.stat_p1(), &self.space);
+        let exp = Exponential::new(multipliers, &self.stat, &self.space);
 
-        grad_f.copy_from_slice(exp.entropy_gradient(10_000, &mut thread_rng()).as_slice());
+        grad_f.copy_from_slice(
+            exp.entropy_gradient(ITERATIONS, &mut thread_rng())
+                .as_slice(),
+        );
         true
     }
 }
@@ -179,19 +216,19 @@ where
     fn constraint(&self, x: &[ipopt::Number], g: &mut [ipopt::Number]) -> bool {
         let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
         multipliers.copy_from_slice(x);
-        let exp = Exponential::new(multipliers, self.stat_p1(), &self.space);
+        let exp = Exponential::new(multipliers, &self.stat, &self.space);
 
         g.copy_from_slice(
-            exp.integral(self.stat_p1(), 10_000, &mut thread_rng())
+            exp.integral(|x| exp.stat_m1(x), ITERATIONS, &mut thread_rng())
                 .as_slice(),
         );
         true
     }
 
     fn constraint_bounds(&self, g_l: &mut [ipopt::Number], g_u: &mut [ipopt::Number]) -> bool {
-        g_l.copy_from_slice(self.g_l.as_slice());
+        g_l[0..N].copy_from_slice(self.g_l.as_slice());
         g_l[N] = 1.;
-        g_u.copy_from_slice(self.g_u.as_slice());
+        g_u[0..N].copy_from_slice(self.g_u.as_slice());
         g_u[N] = 1.;
         true
     }
@@ -210,15 +247,15 @@ where
     fn constraint_jacobian_values(&self, x: &[ipopt::Number], vals: &mut [ipopt::Number]) -> bool {
         let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
         multipliers.copy_from_slice(x);
-        let exp = Exponential::new(multipliers, self.stat_p1(), &self.space);
+        let exp = Exponential::new(multipliers, &self.stat, &self.space);
 
         vals.copy_from_slice(
             exp.integral(
                 |x| {
-                    let stat = (exp.stat)(x);
+                    let stat = exp.stat_m1(x);
                     stat * stat.transpose()
                 },
-                10_000,
+                ITERATIONS,
                 &mut thread_rng(),
             )
             .as_slice(),
@@ -246,7 +283,7 @@ where
     ) -> bool {
         let mut multipliers: SVector<f64, { N + 1 }> = SVector::zeros();
         multipliers.copy_from_slice(x);
-        let exp = Exponential::new(multipliers, self.stat_p1(), &self.space);
+        let exp = Exponential::new(multipliers, &self.stat, &self.space);
 
         let mut lambdas: SVector<f64, { N + 1 }> = SVector::zeros();
         lambdas.copy_from_slice(lambda);
@@ -254,12 +291,12 @@ where
         vals.copy_from_slice(
             exp.integral(
                 |x| {
-                    let stat = (exp.stat)(x);
+                    let stat = exp.stat_m1(x);
                     ((lambdas - obj_factor * multipliers).dot(&stat) - 2. * obj_factor)
                         * stat
                         * stat.transpose()
                 },
-                10_000,
+                ITERATIONS,
                 &mut thread_rng(),
             )
             .as_slice(),
